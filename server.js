f322e8f0d37a2c9ad1bcba5c699e4493bac8c9d9
Dylan Sharon <reachdylan@gmail.com>
2021-04-15 17:23:42 -0400

    testing cors in production environment

--- server.js
+++ server.js
@@ -29,8 +29,7 @@ const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
-app.use(cors({origin: "https://flubbsweeper-front-end.herokuapp.com/"}));
-app.options("/api/board", cors({origin: "https://flubbsweeper-front-end.herokuapp.com/"}));
+app.use(cors());
 
 app.use("/api/board", require("./routes/api/boards"));
 
