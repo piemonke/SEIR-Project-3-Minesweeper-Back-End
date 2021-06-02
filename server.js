5080b348245656f89f24cfcdee0f4d0e2de21ca3
Dylan Sharon <reachdylan@gmail.com>
2021-06-02 17:11:25 -0400

    fixing cors, again

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
-app.use(cors({origin: *}));
+app.use(cors({origin: "https://flubbsweeper-front-end.herokuapp.com/"}));
 
 app.use("/api/board", require("./routes/api/boards"));
 
